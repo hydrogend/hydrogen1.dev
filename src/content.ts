a1ece4392c6a8bbcbdff5e24a4f8c4d38a19d8a3
hydrogend <dtiars.1.hydrogen@gmail.com>
2024-11-17 03:16:01 +0900

    Add years

--- src/content.ts
+++ src/content.ts
@@ -2,14 +2,23 @@ import { type CollectionEntry, getCollection } from "astro:content";
 
 export const allBlogPosts = await getCollection("blog", ({data}) => import.meta.env.PROD ? !data.draft : true);
 
+export type BlogPostMeta = CollectionEntry<"blog">;
 export type BlogPost = CollectionEntry<"blog">['data'];
 
-export const allTags: { tag: string, count: number }[] = (() => {
-    const tagMap = allBlogPosts.flatMap(post => post.data.tags)
-        .reduce((map, tag) => map.set(tag, (map.get(tag) ?? 0) + 1), new Map<string, number>());
-    return [...tagMap].map(([tag, count]) => ({ tag, count }));
-})();
+export const allTags: { tag: string, count: number }[] = 
+    Array.from(allBlogPosts.flatMap(post => post.data.tags)
+        .reduce((map, tag) => map.set(tag, (map.get(tag) ?? 0) + 1), new Map<string, number>())
+        .entries())
+    .map(([tag, count]) => ({ tag, count }));
 
-export function getBlogPostsByTag(tag: string) {
+export function getBlogPostsByTag(tag: string): CollectionEntry<"blog">[] {
     return allBlogPosts.filter(post => post.data.tags.includes(tag));
+}
+
+export function digestSlug(slug: string): { year: number, rest: string } {
+    const match = slug.match(/^(\d{4})\/([^\/]+)$/);
+    if (!match) throw new Error(`Invalid slug: ${slug}`);
+    const [, yearStr, rest] = match;
+    const year = parseInt(yearStr);
+    return { year, rest };
 }
---
+++ src/content/blog/2024/my-first-page/index.md
@@ -0,0 +1,37 @@
+---
+title: "テスト用ページ"
+description: "これはテスト用ページです"
+post: "2024-11-16T18:00"
+tags: ["Test"]
+draft: true
+---
+
+# 見出し1
+
+## 見出し2
+
+### 見出し3
+
+本文
+
+- リスト1
+- リスト2
+- リスト3
+
+1. 第一要素
+2. 第二要素
+
+文中コード`sample`
+
+```java
+// Javaコードのサンプル
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+```
+
+# 別の見出し
+
+本文本文本文本文本文本文本文本文本文本文Latin本文本文本文本文本文本文本文本文本文本文本文本文本文本文
