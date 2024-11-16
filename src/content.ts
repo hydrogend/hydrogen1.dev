867aea5a2003cf8f3abcaf36fe7462d717d3759e
hydrogend <dtiars.1.hydrogen@gmail.com>
2024-11-16 17:07:55 +0900

    Add reference to tag data

--- src/content.ts
+++ src/content.ts
@@ -4,3 +4,12 @@ export const allBlogPosts = await getCollection("blog", ({data}) => import.meta.
 
 export type BlogPost = CollectionEntry<"blog">['data'];
 
+export const allTags: { tag: string, count: number }[] = (() => {
+    const tagMap = allBlogPosts.flatMap(post => post.data.tags)
+        .reduce((map, tag) => map.set(tag, (map.get(tag) ?? 0) + 1), new Map<string, number>());
+    return [...tagMap].map(([tag, count]) => ({ tag, count }));
+})();
+
+export function getBlogPostsByTag(tag: string) {
+    return allBlogPosts.filter(post => post.data.tags.includes(tag));
+}
